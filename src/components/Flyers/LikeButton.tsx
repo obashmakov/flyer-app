e2aaf40c7d0875389c413f2bd26fbc0445794d6a
Olexiy <olexiybashmakov@gmail.com>
2022-03-27 14:56:38 +0300

    [likes] like button fix

--- src/components/Flyers/LikeButton.tsx
+++ src/components/Flyers/LikeButton.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import cx from 'classnames';
 
 import { FlyerProps, LikeButtonProps } from 'types/reducers.interface';
 
 function LikeButton({ id, title }: LikeButtonProps): JSX.Element {
+  const [isLiked, setIsLiked] = useState(false);
+
+  useEffect(() => {
+    const flyersFromLocalStorage: FlyerProps[] = JSON.parse(
+      window.localStorage.getItem('likedFlyers') || '[]',
+    );
+    const isFlyerInLocalStorage = flyersFromLocalStorage.some(
+      (flyer) => flyer.id === id,
+    );
+
+    setIsLiked(isFlyerInLocalStorage);
+  }, [id, isLiked]);
+
   const handleClick = () => {
+    setIsLiked(!isLiked);
     const flyersFromLocalStorage: FlyerProps[] = JSON.parse(
       window.localStorage.getItem('likedFlyers') || '[]',
     );
@@ -30,7 +45,14 @@ function LikeButton({ id, title }: LikeButtonProps): JSX.Element {
 
   return (
     <button className="flyer__button" onClick={handleClick}>
-      <i className="fa-solid fa-heart flyer__heartIcon" />
+      <i
+        className={cx({
+          'fa-solid': true,
+          'fa-heart': true,
+          flyer__heartIcon: true,
+          'flyer__heartIcon--liked': isLiked,
+        })}
+      />
     </button>
   );
 }
